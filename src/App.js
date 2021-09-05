5eda7f426a4ae02e9a9833a4a2774a0b939b9b2d
zerofe13 <zerofe13@gmail.com>
2021-09-05 12:34:14 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import Counter from './Counter';
 import Info from './info';
 
 const App = () => {
-  const [visible, setVisibel] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisibel(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Info />;
 };
 
 export default App;
--- src/info.js
+++ src/info.js
@@ -1,29 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-  useEffect(() => {
-    console.log('랜더링이 완료되었습니다.');
-    console.log({
-      name,
-      nickname,
-    });
-    return () => {
-      console.log('cleanup');
-    };
-  }, []);
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-  const onChangeNikcname = (e) => {
-    setNickname(e.target.value);
+  const [state, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: '',
+  });
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
   };
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName}></input>
-        <input value={nickname} onChange={onChangeNikcname}></input>
+        <input name="name" value={name} onChange={onChange}></input>
+        <input name="nickname" value={nickname} onChange={onChange}></input>
       </div>
       <div>
         <div>

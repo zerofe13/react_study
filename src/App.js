6a8c01d93474da69e2e8bf56a58bb0e72aa47b38
zerofe13 <zerofe13@gmail.com>
2021-09-14 17:03:50 +0900

    immer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,89 @@
 import './App.css';
-import React, { Component, useState } from 'react';
-import Counter from './Counter';
-import Info from './info';
-import Average from './Average';
+import React, { useCallback, useRef, useState } from 'react';
+import produce from 'immer';
 
 const App = () => {
-  return <Average />;
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: '', username: '' });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null,
+  });
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setForm(
+        produce(form, (draft) => {
+          draft[name] = value;
+        })
+      );
+    },
+
+    [form]
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username,
+      };
+      setData(
+        produce(data, (draft) => {
+          draft.array.push(info);
+        })
+      );
+      setForm({
+        name: '',
+        username: '',
+      });
+      nextId.current += 1;
+    },
+    [data, form.name, form.username]
+  );
+  const onRemove = useCallback(
+    (id) => {
+      setData(
+        produce(data, (draft) => {
+          draft.array.splice(
+            draft.array.findIndex((info) => info.id === id),
+            1
+          );
+        })
+      );
+    },
+    [data]
+  );
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="username"
+          placeholder="아이디"
+          vaulue={form.username}
+          onChange={onChange}
+        />
+        <input
+          name="name"
+          placeholder="이름"
+          vaulue={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map((info) => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username}({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default App;

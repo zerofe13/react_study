cb31c3fb328a9995c1308a7bb2ef6781c7d41aaa
zerofe13 <zerofe13@gmail.com>
2021-09-16 15:19:03 +0900

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,87 +1,32 @@
 import './App.css';
 import React, { useCallback, useRef, useState } from 'react';
 import produce from 'immer';
+import Home from './home';
+import About from './About';
+import { Route, Link } from 'react-router-dom';
+import Profile from './Profile';
 
 const App = () => {
-  const nextId = useRef(1);
-  const [form, setForm] = useState({ name: '', username: '' });
-  const [data, setData] = useState({
-    array: [],
-    uselessValue: null,
-  });
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setForm(
-        produce(form, (draft) => {
-          draft[name] = value;
-        })
-      );
-    },
-
-    [form]
-  );
-
-  const onSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
-      const info = {
-        id: nextId.current,
-        name: form.name,
-        username: form.username,
-      };
-      setData(
-        produce(data, (draft) => {
-          draft.array.push(info);
-        })
-      );
-      setForm({
-        name: '',
-        username: '',
-      });
-      nextId.current += 1;
-    },
-    [data, form.name, form.username]
-  );
-  const onRemove = useCallback(
-    (id) => {
-      setData(
-        produce(data, (draft) => {
-          draft.array.splice(
-            draft.array.findIndex((info) => info.id === id),
-            1
-          );
-        })
-      );
-    },
-    [data]
-  );
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          name="username"
-          placeholder="아이디"
-          vaulue={form.username}
-          onChange={onChange}
-        />
-        <input
-          name="name"
-          placeholder="이름"
-          vaulue={form.name}
-          onChange={onChange}
-        />
-        <button type="submit">등록</button>
-      </form>
-      <div>
-        <ul>
-          {data.array.map((info) => (
-            <li key={info.id} onClick={() => onRemove(info.id)}>
-              {info.username}({info.name})
-            </li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/About">소개</Link>
+        </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/About" component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 };

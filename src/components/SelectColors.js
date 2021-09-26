29c37c13b8e17417be3aeb122d6e65e2eeae2237
zerofe13 <zerofe13@gmail.com>
2021-09-26 16:40:11 +0900

    static contextType

--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,37 +1,43 @@
 import React from 'react';
-import { ColorConsumer } from '../contexts/color';
+import { Component } from 'react/cjs/react.production.min';
+import ColorContext, { ColorConsumer } from '../contexts/color';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-const SelectColors = () => {
-  return (
-    <div>
-      <h2>색상을 선택하세요.</h2>
-      <ColorConsumer>
-        {({ actions }) => (
-          <div style={{ display: 'flex' }}>
-            {colors.map((color) => (
-              <div
-                key={color}
-                style={{
-                  background: color,
-                  width: '24px',
-                  height: '24px',
-                  cursor: 'pointer',
-                }}
-                onClick={() => actions.setColor(color)}
-                // 마우스 오른쪽 클린
-                onContextMenu={(e) => {
-                  e.preventDefault(); //마우스 오른쪽 버튼 눌러도 메뉴 안뜨게
-                  actions.setSubColor(color);
-                }}
-              />
-            ))}
-          </div>
-        )}
-      </ColorConsumer>
-      <hr />
-    </div>
-  );
-};
+class SelectColors extends Component {
+  static contextType = ColorContext;
+
+  handleSetColor = (color) => {
+    this.context.actions.setColor(color);
+  };
+  handleSetSubColor = (subcolor) => {
+    this.context.actions.setSubColor(subcolor);
+  };
+  render() {
+    return (
+      <div>
+        <h2>색상을 선택하세요.</h2>
+        <div style={{ display: 'flex' }}>
+          {colors.map((color) => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer',
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMenu={(e) => {
+                e.preventDefault();
+                this.handleSetSubColor(color);
+              }}
+            />
+          ))}
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
 export default SelectColors;
